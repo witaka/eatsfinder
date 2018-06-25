b0164cfe850eb68e1d92e8c9952a908091d36b0c
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-24 21:14:34 -0700

    fix bug 'has_secure_password doesnt work on JSON'

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,10 +2,12 @@ class V1::UsersController < ApplicationController
 before_action :find_user, only: [:show, :update, :destroy]
 
 def create
-  @user = User.new user_params
+  new_parms = user_params
+  new_parms[:password] = params[:password]
+  new_parms[:password_confirmation] = params[:password_confirmation]
+  @user = User.create new_parms
   if @user.save
     render json: @user
-    session[:user_id] = @user.id
   else
     render json: { errors: @user.errors.full_messages }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@ class User < ApplicationRecord
   has_secure_password()
   has_many :providers, dependent: :destroy
   has_many :dishes, dependent: :destroy
-
-  validates :user_name, :full_name, presence: true
+  
+  validates :user_name, :full_name, presence: true, uniqueness: {case_sensitive: false}
   
   validates(
     :email,
     presence: true,
     uniqueness: true,
+    uniqueness: {case_sensitive: false},
     format: {
       with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
     }
   )
+
+  after_validation { self.errors.messages.delete(:password_digest) }
   
 end
